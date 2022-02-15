6c080388470a30d5ffa70a5b7fa24e5fb94d5114
Lucas Camargo <lucas.camargo.silva98@gmail.com>
2022-02-15 19:42:16 -0300

    module build

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,13 +4,18 @@ import { PostagemComponent } from '../components/postagem/postagem.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { NgxMaskModule } from 'ngx-mask';
+import { IonicModule } from '@ionic/angular';
+import { ImageBigscreenPageModule } from '../pages/postagem-modal/image-bigscreen/image-bigscreen.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 
 @NgModule({
   imports: [CommonModule, MatFormFieldModule,
-    MatInputModule,  NgxMaskModule.forRoot(),],
+    MatInputModule,  NgxMaskModule.forRoot(),
+    IonicModule.forRoot(),
+    ImageBigscreenPageModule, FormsModule],
   declarations: [PostagemComponent],
   exports: [
     CommonModule, PostagemComponent
